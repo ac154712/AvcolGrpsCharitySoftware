d3a39f10626284b723ca2ddc49d1d0ae2be794f2
ac154712 <ac154712@8CG8163L3P>
2024-03-27 14:40:22 +1300

    Add project files.

---
+++ AvcolGrpsCharitySoftware/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AvcolGrpsCharitySoftware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
