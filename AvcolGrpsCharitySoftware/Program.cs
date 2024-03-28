5678ee4fdc4aead1c1e8624d2602567ca93ad0cd
Ronald Arzadon <ac154712@avcol.school.nz>
2024-03-28 13:54:06 +1300

    added identity

--- AvcolGrpsCharitySoftware/Program.cs
+++ AvcolGrpsCharitySoftware/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AvcolGrpsCharitySoftware.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AvcolGrpsCharitySoftwareContextConnection") ?? throw new InvalidOperationException("Connection string 'AvcolGrpsCharitySoftwareContextConnection' not found.");
+
+builder.Services.AddDbContext<AvcolGrpsCharitySoftwareContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<AvcolGrpsCharitySoftwareUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AvcolGrpsCharitySoftwareContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
